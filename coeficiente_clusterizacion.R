8c97bd3a3d8c3936f35780cde8e0fc0567f44db6
marialasa <62130723+marialasa@users.noreply.github.com>
2020-04-17 15:10:43 -0300

    Update coeficiente_clusterizacion.R

--- coeficiente_clusterizacion.R
+++ coeficiente_clusterizacion.R
@@ -1,10 +1,11 @@
-# Una vez construido el plot, puede calcularse el
-# Coeficiente de Clusterización de cada red.
+# Una vez ponderadas las redes, puede calcularse el
+# Coeficiente de Clusterización de cada una de ellas.
 
 # Cargar library
 library(igraph)
 
-# Puede calcularse el coeficiente global, promedio o local.
+# De acuerdo a tus necesidades, podés calcular
+# coeficiente global, promedio o local.
 
 G = graph_from_data_frame(d = edge_list, vertices = node_list, directed = FALSE)
     transitivity(G, type="global")
